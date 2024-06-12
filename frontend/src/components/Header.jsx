ed600060bf97988ce20ab0df382f1f4c0b422662
warlord97 <88074251+warlord97@users.noreply.github.com>
2024-06-12 12:17:07 +0530

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
+
+function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+
+  return (
+    <Flex justifyContent={"space-between"} mt={6} mb={12}>
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={6}
+        mx={"auto"}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        onClick={toggleColorMode}
+      />
+
+      {user && (
+        <Flex alignItems={"center"} gap={4}>
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <RxAvatar size={24} />
+          </Link>
+          <Link as={RouterLink} to={`/chat`}>
+            <BsFillChatQuoteFill size={20} />
+          </Link>
+          <Link as={RouterLink} to={`/settings`}>
+            <MdOutlineSettings size={20} />
+          </Link>
+          <Button size={"xs"} onClick={logout} py={4} px={2}>
+            <FiLogOut size={20} />
+          </Button>
+        </Flex>
+      )}
+    </Flex>
+  );
+}
+
+export default Header;
